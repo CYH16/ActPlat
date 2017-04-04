f8df5a01e07b12c8052bef3fb5301ddb5e9150f0
CYH16 <h5s12s20s@yahoo.com.tw>
2017-04-04 20:43:13 +0800

    12. Update

--- activity_platform/static/js/hover.js
+++ activity_platform/static/js/hover.js
@@ -9,7 +9,7 @@ $(function(){
 	function(){
 		$(this).css("background","rgba(240, 255, 255, 0)");
 		$(this).css("color","rgba(240, 255, 255, 1)");
-		$(this).children(".text").children("#time").css("color","rgba(240, 255, 255, .4)");
+		$(this).children(".text").children("#time").css("color","rgba(240, 255, 255, 1)");
 		$(this).children(".text").children("#brief").css("color","rgba(240, 255, 255, .4)");
 	});
 });
--- activity_platform/static/js/loading.js
+++ activity_platform/static/js/loading.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	intro.style.height = $(window).height()+"px";
-	$("#loader").delay(1500).fadeOut(500);
-	$("#welcome").delay(2000).fadeIn(1000);
-	$("#notification").delay(3000).fadeIn(1000);
-	$("#godown").delay(3000).fadeIn(1000);
-	$("#container").delay(3000).fadeIn(1000);
-	$("#footer").delay(3000).fadeIn(1000);
+	$("#loader").delay(1000).fadeOut(500);
+	$("#welcome").delay(1500).fadeIn(1000);
+	$("#notification").delay(2000).fadeIn(1000);
+	$("#godown").delay(2000).fadeIn(1000);
+	$("#container").delay(2000).fadeIn(1000);
+	$("#footer").delay(2000).fadeIn(1000);
  }
 )
