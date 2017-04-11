e3e136df2d90ebd6a510fc7f186f5fb755f01637
CYH16 <h5s12s20s@yahoo.com.tw>
2017-04-11 20:09:19 +0800

    16. Login and like

--- activity_platform/static/js/hover.js
+++ activity_platform/static/js/hover.js
@@ -13,3 +13,15 @@ $(function(){
 		$(this).children(".text").children("#brief").css("color","rgba(240, 255, 255, .4)");
 	});
 });
+
+$(function(){
+	$(".p1 > .title > form > input").hover(
+	function(){
+		$(this).css("background","rgba(240, 255, 255, 1)");
+		$(this).css("color","rgb(15,24,43)");
+	},
+	function(){
+		$(this).css("background","rgba(240, 255, 255, 0)");
+		$(this).css("color","rgba(240, 255, 255, 1)");
+	});
+});
